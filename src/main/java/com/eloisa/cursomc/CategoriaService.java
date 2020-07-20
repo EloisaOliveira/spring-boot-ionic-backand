5a382d493b3a6e8e584deeb08f1046fb4814aeac
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2020-07-20 15:20:31 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/eloisa/cursomc/CategoriaService.java
@@ -0,0 +1,20 @@
+package com.eloisa.cursomc;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+
+    @Autowired
+    private CategoriaRepository repo;
+
+    public Categoria buscar(Integer id){
+        Optional<Categoria> obj = repo.findById(id);
+        return obj.orElse(null);
+
+    }
+    
+}
