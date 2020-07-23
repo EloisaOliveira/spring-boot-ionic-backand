4414b185364711bb544c0185cc006bf07d52cd12
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2020-07-22 22:23:32 -0300

    Atualização: utilizando somente JsonIgnore

--- src/main/java/com/eloisa/cursomc/domain/Categoria.java
+++ src/main/java/com/eloisa/cursomc/domain/Categoria.java
@@ -8,8 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,7 +22,6 @@ public class Categoria implements Serializable {
     private Integer id;
     private String nome;
 
-    @JsonBackReference
     @ManyToMany(mappedBy="categorias")
     private List<Produto> produtos = new ArrayList<>();
 
