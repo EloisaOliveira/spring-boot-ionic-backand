bcf8b453b3518c5366a5ce0f28fe4fb86eba6de1
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2020-09-08 12:32:58 -0300

    Criando profile de desenvolvimento

--- src/main/java/com/eloisa/cursomc/DemoApplication.java
+++ src/main/java/com/eloisa/cursomc/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
+public class DemoApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
