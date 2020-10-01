cae7ca95ef640377e92698dbfe70928bb25900ab
EloisaOliveira <eloisa.oliveirasantos@gmail.com>
2020-09-30 23:16:01 -0300

    Esqueci a senha

--- src/main/java/com/eloisa/cursomc/services/EmailService.java
+++ src/main/java/com/eloisa/cursomc/services/EmailService.java
@@ -1,17 +1,16 @@
 package com.eloisa.cursomc.services;
 
-import javax.mail.internet.MimeMessage;
-
 import org.springframework.mail.SimpleMailMessage;
 
+import com.eloisa.cursomc.domain.Cliente;
 import com.eloisa.cursomc.domain.Pedido;
 
 public interface EmailService {
 
     void sendOrderConfirmationEmail(Pedido obj);
+
     void sendEmail(SimpleMailMessage msg);
 
-    void sendOrderConfirmationHtmlEmail(Pedido obj);
-    void sendHtmlEmail(MimeMessage msg);
+    void sendNewPasswordEmail(Cliente cliente, String newPass);
     
 }
